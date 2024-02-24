55c429ae58f0374b95aa2310f1a3414e972171b8
zulalc <83912055+zulalc@users.noreply.github.com>
2024-02-24 17:53:37 +0300

    Add files via upload

---
+++ src/Components/TodoItems.jsx
@@ -0,0 +1,44 @@
+import './CSS/TodoItems.css'
+import tick from './Assets/tick.png'
+import empty from './Assets/empty.png'
+import cross from './Assets/cross.png'
+const TodoItems = ({no , display, text, setTodos}) => {
+
+const deleteTodo = (no) => {
+  let data = JSON.parse(localStorage.getItem("todos"));
+  data = data.filter((todo) => todo.no!==no);
+  setTodos(data);
+}
+
+
+  const toggle = (no) => {
+    let data = JSON.parse(localStorage.getItem("todos"));
+    for(let i=0; i<data.length;i++){
+      if (data[i].no === no) {
+        if (data[i].display==="") {
+          data[i].display = "line-through"
+        }
+        else {
+          data[i].display = ""
+        }
+        break;
+      }
+    }
+
+    setTodos(data);
+
+  }
+
+  return (
+    <div className='todoitems'>
+      <div className={`todoitems-container ${display}`} onClick={() =>{toggle(no)}}>
+        {display===""?<img src={empty} alt="" />:<img src={tick} alt="" />}
+        
+        <div className="todoitems-text">{text}</div>
+      </div>
+      <img className='todoitems-crossicon' onClick={()=>{deleteTodo(no)}} src={cross} alt="" />
+    </div>
+  )
+}
+
+export default TodoItems
